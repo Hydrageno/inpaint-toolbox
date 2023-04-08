d3863d77984993bfb3287727654f0e8a41234096
Hydrageno <3062055813@qq.com>
2023-04-09 00:28:18 +0800

    add zh and en language selection.

---
+++ src/components/language/en.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'en',
+    projectText: {
+        navigatorText: {
+            traditionInpaintNavigatorText: "Tradition Inpaint",
+            superResolutionNavigatorText: "Super Resolution",
+            removalInpaintNavigatorText: "Removal Inpaint",
+            segmentationInpaintNavigatorText: "Segmentation Inpaint"
+        }
+    }
+}
