0b46f1eae88e58e88fd96ba6f0f4e0c67379cfe6
Hydrageno <3062055813@qq.com>
2023-04-08 20:44:39 +0800

    allow cross domain access

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "./",
+  devServer: {
+    proxy: {
+      ['/dev-api']: {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + '/dev-api']: ''
+        }
+      }
+    }
+  },
 })
