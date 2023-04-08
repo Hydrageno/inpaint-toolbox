b34b5e39a5324ec90a9eff7bffd4aa89897691c3
Hydrageno <3062055813@qq.com>
2023-04-08 20:53:07 +0800

    add element-plus and use

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// create Application instance
+const app = createApp(App);
+// use vuex
+app.use(store);
+// use vue-router to realize page-jump
+app.use(router);
+// use element-plus ui designer for vue3 project
+app.use(ElementPlus);
+// mount the Application
+app.mount("#app");
+
