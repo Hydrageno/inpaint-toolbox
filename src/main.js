48a6000b6e2feb475c90828b4ac1dd77dd653af3
Hydrageno <3062055813@qq.com>
2023-04-10 00:24:15 +0800

    import ElUpload for UploadImage.vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import 'element-plus/dist/index.css'
-import {ElSwitch, ElIcon, ElLink} from 'element-plus'
+import {ElSwitch, ElIcon, ElLink, ElUpload} from 'element-plus'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -27,6 +27,7 @@ app.use(i18n);
 app.component(ElSwitch.name, ElSwitch);
 app.component(ElIcon.name, ElIcon);
 app.component(ElLink.name, ElLink);
+app.component(ElUpload.name, ElUpload);
 // mount the Application
 app.mount("#app");
 
