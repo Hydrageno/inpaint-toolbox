71edb151246719008a8e6d7964e8a5a35f954418
Hydrageno <3062055813@qq.com>
2023-04-10 01:57:47 +0800

    add attribute & method for store.

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    imageURL: null,
+    imageHeight: 0,
+    imageWidth: 0
   },
   getters: {
   },
   mutations: {
+    setImageURL(state, imageURL){
+      state.imageURL = imageURL;
+    },
+    setImageSize(state, imageArr){
+      state.imageHeight = imageArr[1];
+      state.imageWidth = imageArr[0];
+    }
   },
   actions: {
   },
