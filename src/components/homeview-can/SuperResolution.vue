45436655726ecd96141c7a476e631d5ed4c80454
Hydrageno <3062055813@qq.com>
2023-04-10 15:22:24 +0800

    set basic content and style for description in SR.

--- src/components/homeview-can/SuperResolution.vue
+++ src/components/homeview-can/SuperResolution.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="super-resolution">
-        <div class="super-resolution-description"></div>
+        <!--create description area for super-resolution-->
+        <!--为超分辨率创建描述区-->
+        <div class="super-resolution-description">
+            <div class="super-resolution-description-content">
+                <img src="@/assets/demo.gif" class="super-resolution-description-animation">
+                <!--add description text for super-resolution-->
+                <!--为超分辨率添加描述性文字-->
+                <div class="super-resolution-description-text">
+                    <span class="super-resolution-description-text-fluorescence">&nbsp;{{ superResolve }}&nbsp;</span><br>
+                    <span>{{ withWantedPic }}</span><br>
+                    <span>{{ customScale }}</span>
+                    <span class="super-resolution-description-text-fluorescence">&nbsp;{{ scaleSequence }}&nbsp;</span>
+                </div>
+            </div>
+        </div>
         <div class="super-resolution-upload-image"></div>
         <div class="super-resolution-use-case"></div>
     </div>
@@ -9,6 +23,22 @@
 <script>
 export default{
     name: 'SuperResolution',
+    computed:{
+        superResolve(){
+            // switch the content follow the value of 'language' attribute in i18n.
+            // 通过i18n中的language属性值来调整展示的内容，如果为zh则显示中文内容，否则显示英文内容。
+            return this.$t('homeview.superResolution.descriptionText.superResolve')
+        },
+        withWantedPic(){
+            return this.$t('homeview.superResolution.descriptionText.withWantedPic')
+        },
+        customScale(){
+            return this.$t('homeview.superResolution.descriptionText.customScale')
+        },
+        scaleSequence(){
+            return this.$t('homeview.superResolution.descriptionText.scaleSequence')
+        }
+    }
 }
 </script>
 
@@ -20,8 +50,49 @@ export default{
     flex-direction: column;
     // height = desciption-height + upload-iamge-height + use-case-height.
     .super-resolution-description{
-        background-color: orange;
+        background-color: rgb(243,244,246);
         flex: 1;
+        // let inner element center horizontal and vertical.
+        // 让内部元素水平和垂直居中。
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+        .super-resolution-description-content{
+            // let inner element center horizontal and vertical.
+            // 让内部元素水平和垂直居中。
+            display: flex;
+            justify-content: center;
+            align-items: center; 
+            .super-resolution-description-animation{
+                // set the style of animation.
+                // 设置演示动画的样式，包括边界、边界样式、背景阴影。
+                border-radius: 30px;
+                border-style: none;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+                width: 25%;
+            }
+            .super-resolution-description-text{
+                // set the style of font.
+                // 设置字体大小和粗细程度。
+                font-weight: bolder;
+                font-size: 1.75em;
+                color: rgb(44, 62, 80);
+                // align the text inside the super-resolution-description-text div to the right.
+                // 让内部的字靠在最右边。
+                text-align: right;
+                // create distance between text and picture.
+                // 文字和图片间保持一定间距
+                margin-left: 10px;
+                .super-resolution-description-text-fluorescence{
+                    // set the style of fluorescence text
+                    // 设置荧光背景样式
+                    background-color: rgba(189, 255, 1, 0.9);
+                    box-shadow:0 2px 4px rgba(0, 0, 0, 0.25);
+                    border-radius: 10px;
+                    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+                }
+            }
+        }
     }
     .super-resolution-upload-image{
         background-color: green;
