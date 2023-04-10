cf46d8070d6b65d5fed4358ecb0825a3bcbcab96
Hydrageno <3062055813@qq.com>
2023-04-10 21:43:09 +0800

    set basic layout for SaliencyEdgeInpaint

--- src/components/homeview-can/SaliencyEdgeInpaint.vue
+++ src/components/homeview-can/SaliencyEdgeInpaint.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="saliency-edge-inpaint"></div>
+    <div class="saliency-edge-inpaint">
+        <!--create description area for saliency-edge-inpaint-->
+        <!--为显著性目标边缘检测创建描述区-->
+        <div class="saliency-edge-inpaint-description"></div>
+        <div class="saliency-edge-inpaint-upload-image"></div>
+        <div class="saliency-edge-inpaint-use-case"></div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,23 @@ export default{
 </script>
 
 <style lang="less">
-
+.saliency-edge-inpaint{
+    // let inner element flow by column
+    // 让内部元素按照竖向排序
+    display: flex;
+    flex-direction: column;
+    // height = desciption-height + upload-iamge-height + use-case-height.
+    .saliency-edge-inpaint-description{
+        background-color: black;
+        flex: 1;
+    }
+    .saliency-edge-inpaint-upload-image{
+        background-color: blue;
+        flex: 2;
+    }
+    .saliency-edge-inpaint-use-case{
+        background-color: red;
+        flex: 2;
+    }
+}
 </style>
