250c7c3a779d36d3187e9b1fb0eacfe3b2d3dd73
Hydrageno <3062055813@qq.com>
2023-04-11 00:07:39 +0800

    build basic structure for TIView

---
+++ src/views/TraditionInpaintView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tradition-inpaint-view">
+        <div class="tradition-inpaint-view-navigator"></div>
+        <div class="tradition-inpaint-view-content" ref="traditionInpaintViewContent"></div>
+        <div class="tradition-inpaint-view-toolbar"></div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'TraditionInpaintView',
+}
+</script>
+
+<style lang="less">
+
+</style>
