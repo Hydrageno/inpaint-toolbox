445ed1900679b256ff8f20fc0a426fe5b8c19f8e
Hydrageno <3062055813@qq.com>
2023-04-11 02:12:00 +0800

    allow jump to TI view

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/tradition-inpaint',
+    name: 'tradition-inpaint',
+    component: () => import('@/views/TraditionInpaintView.vue')
+  }
   // {
   //   path: '/about',
   //   name: 'about',
