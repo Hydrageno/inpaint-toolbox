64fbd7c3ef9f5480b167667bb0a9780f38a1171a
Hydrageno <3062055813@qq.com>
2023-04-11 15:27:35 +0800

    lan pack v1.0

--- src/components/language/en.js
+++ src/components/language/en.js
@@ -1,11 +1,92 @@
 export default {
     name: 'en',
     projectText: {
-        navigatorText: {
-            traditionInpaintNavigatorText: "Tradition Inpaint",
-            superResolutionNavigatorText: "Super Resolution",
-            removalInpaintNavigatorText: "Removal Inpaint",
-            segmentationInpaintNavigatorText: "Segmentation Inpaint"
+        homeview: {
+            navigatorText: {
+                traditionInpaintNavigatorText: "Tradition Inpaint",
+                superResolutionNavigatorText: "Super Resolution",
+                removalInpaintNavigatorText: "Removal Inpaint",
+                saliencyEdgeInpaintNavigatorText: "SaliencyEdge Inpaint"
+            },
+            traditionInpaint: {
+                descriptionText: {
+                    front: "Repaint wanted",
+                    object: "object",
+                    people: "people",
+                    scenery: "scenery",
+                    animation: "animation",
+                    later: "from your pictures in seconds",
+                    helpful: "Might be helpful~"
+                },
+                useCase: "—— Use - Case ——"
+            },
+            superResolution: {
+                descriptionText:{
+                    superResolve: "Super Resolve",
+                    withWantedPic: "Upload Wanted Image",
+                    customScale: "Custom Scale:",
+                    scaleSequence: "X2、X3.5、X4"
+                },
+                useCase: "——  Tutorial  ——"
+            },
+            removalInpaint: {
+                descriptionText:{
+                    picHave: "Do photos have",
+                    waterMark: "WATERMARK",
+                    stains: "STAINS",
+                    questionMark: "?",
+                    tryTool: "Come and Try",
+                    deWaterMarkStains: "WATERMARK & STAINS REMOVAL",
+                    endTool: "tool"
+                },
+                useCase: "——  Example  ——"
+            },
+            saliencyEdgeInpaint:{
+                descriptionText:{
+                    saliencyObject: "SaliencyObject?",
+                    saliencyEdge: "Edge of SaliencyObject",
+                    damage: "is damaged and hard to witness",
+                    or: "or want to",
+                    get: "download",
+                    tryTool: "Come and Try it!"
+                },
+                useCase: "—— Use - Case ——"
+            }
+        },
+        uploadImage: {
+            customUpload:{
+                uploadImageGuide: "upload image",
+                uploadImageDescription: "Drag or Click to Upload",
+                typeLimit: "SORRY BUT TYPE LIMIT: .jpeg .jpg .png"
+            },
+            providedUpload: "Or try with these examples ↓",
+
+        },
+        traditionInpaintView: {
+            navigatorText:{
+                backHome: "Back Home"
+            },
+            toolBar:{
+                penThickness: "Pen Thickness:",
+                eraseText: "ERASE",
+                penText: "PAINT",
+                selectMode: "Select Mode",
+                celehq: "Human Face",
+                places: "Scenary",
+                imageNet: "Other",
+                animation: "Animated Character",
+                submit: "SUBMIT",
+                download: "DOWNLOAD"
+            },
+        },
+        superResolutionView: {
+            navigatorText: {
+                backHome: "Back Home"
+            },
+            toolBar:{
+                submit: "SUBMIT",
+                download: "DOWNLOAD"
+            }
         }
     }
 }
--- src/components/language/zh.js
+++ src/components/language/zh.js
@@ -1,11 +1,92 @@
 export default {
-    name: 'en',
+    name: 'zh',
     projectText: {
-        navigatorText: {
-            traditionInpaintNavigatorText: "Tradition Inpaint",
-            superResolutionNavigatorText: "Super Resolution",
-            removalInpaintNavigatorText: "Removal Inpaint",
-            segmentationInpaintNavigatorText: "Segmentation Inpaint"
+        homeview: {
+            navigatorText: {
+                traditionInpaintNavigatorText: "图像修复",
+                superResolutionNavigatorText: "超分辨率",
+                removalInpaintNavigatorText: "去除水印",
+                saliencyEdgeInpaintNavigatorText: "边缘修复"
+            },
+            traditionInpaint: {
+                descriptionText: {
+                    front: "修复图片中",
+                    object: "  物 品  ",
+                    people: "  人 物  ",
+                    scenery: "  背 景  ",
+                    animation: "动漫人物",
+                    later: "在即刻间。",
+                    helpful: "可能会有帮助~"
+                },
+                useCase: "—— 示 - 例 ——",
+            },
+            superResolution: {
+                descriptionText:{
+                    superResolve: "超分辨率",
+                    withWantedPic: "上传待操作的图片",
+                    customScale: "体验不同尺寸：",
+                    scaleSequence: "X2、X3.5以及X4"
+                },
+                useCase: "—— 教 - 程 ——"
+            },
+            removalInpaint: {
+                descriptionText:{
+                    picHave: "照片有",
+                    waterMark: "水印",
+                    stains: "污渍",
+                    questionMark: "？",
+                    tryTool: "快来试试",
+                    deWaterMarkStains: "去水印、去污渍",
+                    endTool: "工具"
+                },
+                useCase: "—— 范 - 例 ——"
+            },
+            saliencyEdgeInpaint:{
+                descriptionText:{
+                    saliencyObject: "显著性目标？",
+                    saliencyEdge: "显著目标边缘",
+                    damage: "有破损？肉眼难以察觉？",
+                    or: "或者想要",
+                    get: "获得",
+                    tryTool: "快来试试这个工具"
+                },
+                useCase: "—— 典 - 范 ——"
+            }
+        },
+        uploadImage: {
+            customUpload:{
+                uploadImageGuide: "上传图片",
+                uploadImageDescription: "拖拽图片或点击上传",
+                typeLimit: "虽然但是文件类型限制: .jpeg .jpg .png"
+            },
+            providedUpload: "或者尝试一下图片↓",
+            
+        },
+        traditionInpaintView: {
+            navigatorText:{
+                backHome: "返回主页"
+            },
+            toolBar:{
+                penThickness: "调整笔粗细：",
+                eraseText: "橡皮擦模式",
+                penText: "绘画模式",
+                selectMode: "选择模式",
+                celehq: "人脸",
+                places: "背景",
+                imageNet: "其它",
+                animation: "动漫人物",
+                submit: "提交",
+                download: "下载"
+            }
+        },
+        superResolutionView: {
+            navigatorText: {
+                backHome: "返回主页"
+            },
+            toolBar:{
+                submit: "提交",
+                download: "下载"
+            }
         }
     }
 }
